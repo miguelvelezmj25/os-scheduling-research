88c7145d5dfb28c11727a6a542c6738a56aff8c8
rslechta <slec0016@stthomas.edu>
2014-11-06 13:57:47 -0600

    Create RandomInputGenerator.java

---
+++ src/com/jsv/RandomInputGenerator.java
@@ -0,0 +1,48 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class RandomInputGenerator {
+	
+	public static void main(String[] args) throws IOException
+	{
+		Random rand = new Random();
+		int num_processes = 4;
+		int num_cpus = 0;
+		int cum_start_time = 0;
+		int rand_start_time = 0;
+		int cpu_time = 0;
+		int oth_time = 0;
+		
+		File file = new File("src/rando.txt");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file,false));
+	
+		
+		for(int i=0;i<num_processes;i++)
+		{
+			rand_start_time = rand.nextInt(100);
+			cum_start_time += rand_start_time;
+			bw.write("NEW "+cum_start_time);
+			bw.newLine();
+			do{
+				num_cpus = rand.nextInt(10);
+			}while(num_cpus == 0);
+			
+			for(int j=0;j<num_cpus;j++)
+			{
+				cpu_time = rand.nextInt(500);
+				oth_time = rand.nextInt(500);
+				bw.write("CPU "+cpu_time);
+				bw.newLine();
+				bw.write("OTH "+oth_time);
+				bw.newLine();
+			}
+			
+		}
+		
+		bw.close();
+	}
+
+}
