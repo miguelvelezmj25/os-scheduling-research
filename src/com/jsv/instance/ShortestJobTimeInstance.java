fdeccdb0cdfd322da2fa48512aecccd9cc3a207f
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-21 09:15:48 -0600

    Refactored compareTo variable

--- src/com/jsv/instance/ShortestJobTimeInstance.java
+++ src/com/jsv/instance/ShortestJobTimeInstance.java
@@ -11,9 +11,14 @@ public class ShortestJobTimeInstance extends Instance {
 	}
 	
 	@Override
-	public int compareTo(Instance o) {
+	/** Comparing the first command in the instance and sorting by that 
+	 * time.
+	 */
+	public int compareTo(Instance instance) {
+		System.out.println("This: " + this.getPid() + " - instance: " + instance.getPid());
+		
 		int thisNextTime = super.getCommandList().get(0).getTimeCommand();
-		int otherNextTime = o.getCommandList().get(0).getTimeCommand();
+		int otherNextTime = instance.getCommandList().get(0).getTimeCommand();
 		
 		if(thisNextTime < otherNextTime) {
 			return -1;
