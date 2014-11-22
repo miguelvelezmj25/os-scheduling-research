473ce19f1a913d83f27d4b0d391cb5895b3e8826
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-22 17:16:47 -0600

    Renamed to shortestTotalTime

---
+++ src/com/jsv/instance/ShortestTotalTime.java
@@ -0,0 +1,34 @@
+package com.jsv.instance;
+
+/**
+ * Sorts by the smallest total time that an Instance needs to execute 
+ */
+public class ShortestTotalTime extends Instance {
+  
+	public ShortestTotalTime(int pid, int startTime) {
+		super(pid, startTime);
+	}
+	 
+	@Override
+	/** Sort by the total time of the Instance.
+	 */
+	// TODO do we want to preempt if a another Instance has a smaller total time
+	public int compareTo(Instance instance) {
+		int thisTotalTime = super.getTotalTime();
+		int otherTotalTime = instance.getTotalTime();
+		
+		System.out.println("PID: " + super.getPid() + " - " + thisTotalTime);
+		System.out.println("PID: " + instance.getPid() + " - " + otherTotalTime);
+		
+		if(thisTotalTime < otherTotalTime) {
+			return -1;
+		}
+		else if(thisTotalTime > otherTotalTime) {
+			return 1;
+		}
+		
+		return 0;
+	}
+
+
+}
